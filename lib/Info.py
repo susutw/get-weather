f62a99d2145147d4baf7acfee1c15e4929a89068
susutw <sudosu.tw@gmail.com>
2019-12-21 13:25:15 +0800

    Change return value of get_weather_by_city function

--- lib/Info.py
+++ lib/Info.py
@@ -12,18 +12,11 @@ def get_weather_by_city(city_name):
     weather_json = res.json()
 
     if weather_json['cod'] == '404':
-        print("沒有這個城市的說(|||ﾟдﾟ)")
-        return False
+        return
     
     weathet_temp = weather_json['main']['temp']
     weather_main = weather_json['weather'][0]['main']
-
-    c_temp = round(weathet_temp - 273.15, 2) # 攝氏
     ts = int(time.time())
-    time_array = time.strftime("%Y/%m/%d %H:%M", time.localtime(ts))
-
-    print('感謝查詢！你要的資訊在下面！(●´ω｀●)ゞ')
-    print("地區：{}，溫度（攝氏）：{}，氣象：{}，查詢時間：{}。".format(city_name, c_temp, weather_main, time_array))
 
     weather_value = {"city": city_name, "temp": weathet_temp, 'main': weather_main, 'timestamp': ts}
     
--- main.py
+++ main.py
@@ -20,10 +20,18 @@ def main():
     if action == feature[0]:
         city_name = input('請輸入要查詢的城市：')
         value = Info.get_weather_by_city(city_name.lower())
-        if value == False:
+        if value == None:
+            print("沒有這個城市的說(|||ﾟдﾟ)")
             return
 
         result = DB.insert_weather_info(value['city'], value['temp'], value['main'], value['timestamp'])
+        
+        c_temp = round(value['temp'] - 273.15, 2) # 攝氏
+        time_array = time.strftime("%Y/%m/%d %H:%M", time.localtime(value['timestamp']))
+
+        print('感謝查詢！你要的資訊在下面！(●´ω｀●)ゞ')
+        print("地區：{}，溫度（攝氏）：{}，氣象：{}，查詢時間：{}。".format(city_name, c_temp, value['main'], time_array))
+
         return
     
     if action == feature[1]:
